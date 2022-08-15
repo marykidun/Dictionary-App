3062fdad26b80935670aa9ddaabcf913f379edd7
marykidun <107813028+marykidun@users.noreply.github.com>
2022-08-15 15:34:47 +0300

    Updated Meaning css a bit

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,17 @@
 import React from "react";
+import "./Meaning.css";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h5>{props.meaning.partOfSpeech}</h5>
+      <h6>{props.meaning.partOfSpeech}</h6>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
+            <p>{definition.definition}</p>
             <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
+              Example: <em>{definition.example}</em>
             </p>
           </div>
         );
